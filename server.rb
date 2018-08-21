b1525ccddb6eb8fc01d017bd6516fb447d354714
Sean McCarthy <sean.mccarthy@fluidintel.com>
2018-08-21 19:52:19 +1000

    Updated server to accept param for mqtt broker IP

--- server.rb
+++ server.rb
@@ -1,11 +1,18 @@
 require 'sinatra'
 
+if ARGV[0].nil?
+  puts 'Usage: ruby server.rb <ip_address_of_mqtt_broker>'
+  exit 1
+end
 set :bind, '0.0.0.0'
 set :public_folder, File.dirname(__FILE__) + '/files'
+set :mqtt_host, ARGV[0]
+set :mqtt_port, ARGV[1] || 8083
+
 get '/' do
-  erb :telemetry, locals: { mqtt_host: '192.168.254.55', mqtt_port: 8083 }
+  erb :telemetry, locals: { mqtt_host: settings.mqtt_host, mqtt_port: settings.mqtt_port }
 end
 
 get '/display' do
-  erb :display, locals: { mqtt_host: '192.168.254.55', mqtt_port: 8083 }
+  erb :display, locals: { mqtt_host: settings.mqtt_host, mqtt_port: settings.mqtt_port }
 end
